34495fc4bc5e200dc525566d221186394231756a
Supreme Imprints <supremeimprints@gmail.com>
2025-07-29 01:48:40 +0000

    [04:44:06.969] Running build in Washington, D.C., USA (East) – iad1 [04:

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,5 @@
 import LandingPage from './(landing)/page';
-import LandingLayout from './(landing)/layout';
 
 export default function Home() {
-  return (
-    <LandingLayout>
-      <LandingPage />
-    </LandingLayout>
-  );
+  return <LandingPage />;
 }
